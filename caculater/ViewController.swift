35e109ef4aea2f5e664849f10691e347f7337b6b
20171105145 <20171105145du@admindeiMac-62.local>
2018-09-28 09:30:32 +0800

    计算器设计
    添加加法，数字

--- caculater/ViewController.swift
+++ caculater/ViewController.swift
@@ -9,12 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var temp = 0
+    var flag = 0
 
+        @IBOutlet weak var Bsee: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func B1(_ sender: Any) {
+        Bsee.text = Bsee.text!+"1"
+            }
+    
+    
+    @IBAction func B2(_ sender: Any) {
+        Bsee.text = Bsee.text!+"2"
+    }
+    
+    
+    @IBAction func B3(_ sender: Any) {
+        Bsee.text = Bsee.text!+"3"
+    }
+    
+    
+    @IBAction func B4(_ sender: Any) {
+        Bsee.text = Bsee.text!+"4"
+    }
+    
+    
+    @IBAction func B5(_ sender: Any) {
+        Bsee.text = Bsee.text!+"5"
+    }
+    
+    
+    @IBAction func B6(_ sender: Any) {
+        Bsee.text = Bsee.text!+"6"
+    }
+    
+    @IBAction func B7(_ sender: Any) {
+        Bsee.text = Bsee.text!+"7"
+    }
+    
+    @IBAction func B8(_ sender: Any) {
+        Bsee.text = Bsee.text!+"8"
+    }
+    
+    @IBAction func B9(_ sender: Any) {
+        Bsee.text = Bsee.text!+"9"
+    }
+    
+    @IBAction func Buttondelete(_ sender: Any) {
+        Bsee.text = ""
+    }
+    
+    @IBAction func Buttonequal(_ sender: Any) {
+        if(flag == 1)
+        {
+            var sum = 0
+            sum = temp + Int(Bsee.text!)!
+            Bsee.text = "\(sum)"
+        }
+    }
+    
+    
+    @IBAction func pluse(_ sender: Any) {
+        temp = Int(Bsee.text!)!
+        Bsee.text = ""
+        flag = 1
+    }
+    
+    
+    
+    
+    
 }
 
